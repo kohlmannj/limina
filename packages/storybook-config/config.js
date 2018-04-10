52dcc6bda51a3e5acabbe34fad654215513a537f
Joseph Kohlmann <joseph.kohlmann@nytimes.com>
2018-04-09 22:05:36 -0400

    remove storybook-config package

--- packages/storybook-config/config.js
+++
@@ -1,9 +0,0 @@
-import { configure } from '@storybook/react';
-
-const req = require.context('../src/components', true, /\.stories\.(js|jsx|ts|tsx)$/);
-
-function loadStories() {
-  req.keys().forEach(filename => req(filename));
-}
-
-configure(loadStories, module);
