c8b38998c33cc3fdf30396813cea0199cc72a2a2
Joseph Kohlmann <joseph.kohlmann@nytimes.com>
2018-04-23 00:24:47 -0400

    Upgrade Babel + get remark + lint-staged working

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,13 +1,14 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const path = require('path');
-const TSDocgenPlugin = require("react-docgen-typescript-webpack-plugin");
+const TSDocgenPlugin = require('react-docgen-typescript-webpack-plugin');
 
 module.exports = (baseConfig, env, config) => {
-    config.module.rules.push({
-        test: /\.(ts|tsx)$/,
-        loader: require.resolve("babel-loader"),
-        options: { extends: path.join(__dirname, '.babelrc')}
-      });
-    config.plugins.push(new TSDocgenPlugin());
-    config.resolve.extensions.push(".ts", ".tsx");
-    return config;
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    loader: require.resolve('babel-loader'),
+    options: { extends: path.join(__dirname, '.babelrc') },
+  });
+  config.plugins.push(new TSDocgenPlugin());
+  config.resolve.extensions.push('.ts', '.tsx');
+  return config;
 };
