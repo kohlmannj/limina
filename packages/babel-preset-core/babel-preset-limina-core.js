ed149316b3d65fd37affd4dff8e49aef4e2d7592
Joseph Kohlmann <kohlmannj@mac.com>
2019-01-27 20:58:37 -0500

    Upgrade deps + configs

--- packages/babel-preset-core/babel-preset-limina-core.js
+++ packages/babel-preset-core/babel-preset-limina-core.js
@@ -1,24 +1,48 @@
 /* eslint-env node */
-const env = process.env.BABEL_ENV || process.env.NODE_ENV;
-
-module.exports = () => ({
+module.exports = api => ({
   presets: [
     [
       '@babel/preset-env',
       {
-        modules: 'umd',
-        targets: {
+        modules: false,
+        target: {
           browsers: ['last 2 versions'],
         },
+        useBuiltIns: 'entry',
       },
     ],
-    ['@babel/preset-stage-1', { decoratorsLegacy: true }],
     '@babel/preset-typescript',
   ],
   plugins: [
+    '@babel/plugin-transform-runtime',
     [
       'babel-plugin-emotion',
-      env === 'production' ? { hoist: true } : { sourceMap: true, autoLabel: true },
+      api.env('production') ? { hoist: true } : { sourceMap: true, autoLabel: true },
     ],
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-syntax-import-meta',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-json-strings',
+    [
+      '@babel/plugin-proposal-decorators',
+      {
+        legacy: true,
+      },
+    ],
+    '@babel/plugin-proposal-function-sent',
+    '@babel/plugin-proposal-export-namespace-from',
+    '@babel/plugin-proposal-numeric-separator',
+    '@babel/plugin-proposal-throw-expressions',
+    '@babel/plugin-proposal-export-default-from',
+    '@babel/plugin-proposal-logical-assignment-operators',
+    '@babel/plugin-proposal-optional-chaining',
+    [
+      '@babel/plugin-proposal-pipeline-operator',
+      {
+        proposal: 'minimal',
+      },
+    ],
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-proposal-do-expressions',
   ],
 });
