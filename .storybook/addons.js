90150dc30f49e2ac84c395e31ab720a846d03f1f
Joseph Kohlmann <kohlmannj@mac.com>
2019-05-25 17:45:26 -0400

    Comment out @storybook/addon-storysource for now

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,4 +1,4 @@
 /* eslint-disable import/no-extraneous-dependencies */
 require('@storybook/addon-knobs/register');
-require('@storybook/addon-storysource/register');
+// require('@storybook/addon-storysource/register');
 require('@storybook/addon-viewport/register');
